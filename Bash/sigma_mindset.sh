d21147700744927c391dc695d659967572f6d4df
ExitEject <150757931+ExitEject@users.noreply.github.com>
2023-11-28 18:43:30 -0800

    a

--- Bash/sigma_mindset.sh
+++ Bash/sigma_mindset.sh
@@ -3,7 +3,7 @@ echo this is basically legion but stupid, dont make fun of me
 read -p "Enter IP Address of target " target
 if ping -c 1 $target 2>/dev/null; then
     echo "$target up"
-    gnome-terminal --bash -c "nmap -sV -A $target;exec bash"
+    gnome-terminal --bash -c "nmap -sV -A $target; exec bash"
     gnome-terminal --bash -c "gobuster dir -u $target:80 -w /usr/share/wordlists/dirb/common.txt;exec bash"
     firefox -P 'default-esr' http://$target:80 https://$target:443
 else
