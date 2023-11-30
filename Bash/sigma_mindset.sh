b9db0547785b18110bc3ef20869f63bf14d75007
ExitEject <150757931+ExitEject@users.noreply.github.com>
2023-11-29 20:30:35 -0800

    removed pop up terminals

--- Bash/sigma_mindset.sh
+++ Bash/sigma_mindset.sh
@@ -3,8 +3,8 @@ echo this is basically legion but stupid, dont make fun of me
 read -p "Enter IP Address of target" target
 if ping -c 1 $target 2>/dev/null; then
     echo "$target up"
-    gnome-terminal -- bash -c "nmap -oN ~/Downloads/nmap.txt -sC -sV -A $target; exec bash"
-    gnome-terminal -- bash -c "gobuster dir -u $target:80 -w /usr/share/wordlists/dirb/common.txt -x php -o ~/Downloads/gobuster.txt; exec bash"
+    gnome-terminal -- bash -c "nmap -oN ~/Downloads/nmap.txt -sC -sV -A $target"
+    gnome-terminal -- bash -c "gobuster dir -u $target:80 -w /usr/share/wordlists/dirb/common.txt -x php -o ~/Downloads/gobuster.txt"
     firefox -P 'default-esr' http://$target:80 https://$target:443
 else
     echo "$target down"
