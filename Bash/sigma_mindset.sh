a13fd8647b795e78617cb011a07d2046caea0507
ExitEject <150757931+ExitEject@users.noreply.github.com>
2023-12-02 10:56:42 -0800

    Update sigma_mindset.sh
    removed two unneeded comments and fixed some formatting

--- Bash/sigma_mindset.sh
+++ Bash/sigma_mindset.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# help
 Help()
 {
 	#display help
@@ -36,12 +35,11 @@ read -p "Enter IP Address of target" target
     	sudo chmod 777 $target.txt
     	sed -r 's/.{7}//' $target.txt > newfile.tmp && mv newfile.tmp $target.txt
     	A=$(echo "$target") 
-    	#>> $target.txt
     	B=$(cat "$target.txt")
     	sudo sed -i "4i $A $B" /etc/hosts
     	gnome-terminal -- bash -c  "nmap -oN ~/Downloads/nmap.txt -p- --min-rate 1000 -sC -sV $target"
     	gnome-terminal -- bash -c  "gobuster dir -u $target:80 -w /usr/share/wordlists/dirb/common.txt -x php -o ~/Downloads/gobuster.txt"
-    firefox -P 'default-esr' http://$target:80 https://$target:443
+    	firefox -P 'default-esr' http://$target:80 https://$target:443
     	rm $target.txt
 else
     echo "$target down"
