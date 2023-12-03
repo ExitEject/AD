3e961502a744f059d8231e982ff9f044e2f3360e
ExitEject <150757931+ExitEject@users.noreply.github.com>
2023-12-03 12:37:28 -0800

    Update sigma_mindset.sh
    a

--- Bash/sigma_mindset.sh
+++ Bash/sigma_mindset.sh
@@ -44,7 +44,7 @@ SigmaMindset()
 	firefox -P 'default-esr' http://$target:80 https://$target:443
     	#opens firefox to 80 and 443 just to check it out
 	rm $target.txt
-	{
+	}
 while getopts ":hcb" option; do
 	case $option in
 	h) #display help
