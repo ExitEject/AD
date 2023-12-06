a560a1f6e30a538c84afd88148b1c4eca732a359
ExitEject <150757931+ExitEject@users.noreply.github.com>
2023-12-06 14:23:43 -0800

    a

---
+++ Powershell/renamecomputerscript.ps1
@@ -0,0 +1,6 @@
+#remove #Domain01\Admin01 and enter the credentials you want to use
+wmic bios get serialnumber >> machine.txt
+$A = Get-Content(machine.txt)
+Rename-Computer -NewName "$A" -DomainCredential #put Domain01\Admin01 -Restart
+Remove-Item machine.txt
+Write-Host "Done."
---
+++ VBA/ExcelCSVMacro.vb
@@ -0,0 +1,5 @@
+Option Explicit
+Private sub SubmitButton_Click()
+    Dim ws As Worksheet
+    Set ws = ThisWorkbook.Sheets("Sheet1") ' Change to your sheet name
+    Dim nextRow As Integer 
