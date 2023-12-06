02f32bee5101f1f547efb690dc79a835ebbd1614
ExitEject <150757931+ExitEject@users.noreply.github.com>
2023-12-06 15:01:25 -0800

    a

--- Powershell/renamecomputerscript.ps1
+++ Powershell/renamecomputerscript.ps1
@@ -1,8 +1,11 @@
 #remove #Domain01\Admin01 and enter the credentials you want to use
+Write-Host "Getting serial number of machine, writing to temporary file."
 wmic bios get serialnumber >> machine.txt
-Get-Content machine.txt-replace 'serialnumber'
-$A = Get-Content(machine.txt)
-Get-Content(machine.txt)
-#Rename-Computer -NewName "CSC-$A" -DomainCredential #put Domain01\Admin01 -Restart
-#Remove-Item machine.txt
-#Write-Host "Done."ls
+# PowerShell
+Get-Content machine.txt | Where-Object {$_ -notmatch "serialnumber"} | Set-Content machine2.txt
+$A = Get-Content(machine2.txt)
+Write-Host "Cleaning up files..."
+Remove-Item machine.txt
+Remove-Item machine2.txt
+Rename-Computer -NewName "CSC-$A" -DomainCredential #put Domain01\Admin01 -Restart
+Write-Host "Done."
