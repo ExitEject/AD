0c762e40d53bf35c3fd85f07d624320fa76d5e9c
ExitEject <150757931+ExitEject@users.noreply.github.com>
2023-12-06 14:58:42 -0800

    a

--- Powershell/renamecomputerscript.ps1
+++ Powershell/renamecomputerscript.ps1
@@ -1,6 +1,8 @@
 #remove #Domain01\Admin01 and enter the credentials you want to use
 wmic bios get serialnumber >> machine.txt
+Get-Content machine.txt-replace 'serialnumber'
 $A = Get-Content(machine.txt)
-Rename-Computer -NewName "$A" -DomainCredential #put Domain01\Admin01 -Restart
-Remove-Item machine.txt
-Write-Host "Done."
+Get-Content(machine.txt)
+#Rename-Computer -NewName "CSC-$A" -DomainCredential #put Domain01\Admin01 -Restart
+#Remove-Item machine.txt
+#Write-Host "Done."ls
